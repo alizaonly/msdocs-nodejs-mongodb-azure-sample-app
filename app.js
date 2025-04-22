a67773c39a0542ff124938f6826b82402d053fff
alizaonly <166973172+alizaonly@users.noreply.github.com>
2025-04-22 03:13:49 +0000

    update environment variablw

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function getApp() {
 
   // Database
   // Use AZURE_COSMOS_CONNECTIONSTRING if available, otherwise fall back to MONGODB_URI
-  const mongoUri = process.env.MONGODB_URI; // For App Service, change to process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_URI;
+  const mongoUri =process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_URI ; // For App Service, change to process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_URI;
 
   mongoose.connect(mongoUri).then(() => {
     console.log('Connected to database');
@@ -32,7 +32,7 @@ async function getApp() {
 
   app.use(logger("dev"));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urprocess.env.MONGODB_URIlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, "public")));
 
